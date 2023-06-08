2d04af197d20209198d3bad9277b035a1b860757
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-06-08 23:51:55 +0530

    Initialised Theme

---
+++ src/theme/fonts.ts
@@ -0,0 +1,19 @@
+const size = {
+  xs: 10,
+  s: 12,
+  default: 14,
+  md: 16,
+  lg: 20,
+  xlg: 24,
+  xxlg: 30,
+};
+
+const weight = {
+  full: '900',
+  bold: 'bold',
+  semi: '600',
+  thin: '400',
+  normal: 'normal',
+};
+
+export {size, weight};
