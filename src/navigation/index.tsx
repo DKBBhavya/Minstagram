379fa761f3866dc06f517aeec874b7714df7f6ac
Bhavya Agarwal <bhavya.dkb@gmail.com>
2023-06-12 09:17:40 +0530

    Initialised Stack Navigator

--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -1,10 +1,42 @@
 import {NavigationContainer} from '@react-navigation/native';
-import PostUploadScreen from '../screens/PostUploadScreen/PostUploadScreen';
+import ProfileScreen from '../screens/ProfileScreen';
+import {createNativeStackNavigator} from '@react-navigation/native-stack';
+import HomeScreen from '../screens/HomeScreen/HomeScreen';
+import {Image} from 'react-native';
+import logo from '../assets/images/logo.png';
+
+const Stack = createNativeStackNavigator();
 
 const Navigation = () => (
   <NavigationContainer>
-    <PostUploadScreen />
+    <Stack.Navigator
+      initialRouteName="Feed"
+      screenOptions={{headerShown: true}}>
+      <Stack.Screen
+        name="Feed"
+        component={HomeScreen}
+        options={{
+          headerTitle: HeaderTitle,
+          headerTitleAlign: 'center',
+        }}
+      />
+      <Stack.Screen
+        name="UserProfile"
+        component={ProfileScreen}
+        options={{title: 'Profile'}}
+      />
+    </Stack.Navigator>
   </NavigationContainer>
 );
+
+const HeaderTitle = () => {
+  return (
+    <Image
+      source={logo}
+      resizeMode="contain"
+      style={{width: 150, height: 40}}
+    />
+  );
+};
 
 export default Navigation;
